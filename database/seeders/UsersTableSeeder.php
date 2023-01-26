c8296cf8ac7829dfa2b3bf402601814d5db8515f
M-OdhieP <tiotio179@gmail.com>
2023-01-26 09:03:27 +0700

    fix seeder again

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -21,7 +21,9 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'Muhamad Odhie prasetio',
             'email' => 'tiotio179@gmail.com',
+            'email_verified_at' => now(),
             'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
         ]);
 
         UserFactory::times(10)->create();
